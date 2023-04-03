de9d1ba37c6aaaa2c8354a77b0c017f0a4779dd3
muky1 <muhamed.begic01@gmail.com>
2023-04-03 13:34:56 +0200

    updated to use axios to fetch data

--- src/pages/ninjas/[id].tsx
+++ src/pages/ninjas/[id].tsx
@@ -1,30 +1,73 @@
+import axios from "axios";
+
 export const getStaticPaths = async() => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await res.json();
+//     const res = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await res.json();
 
-    const paths = data.map((ninja: any) => {
+//     const paths = data.map((ninja: any) => {
+//         return {
+//             params: { id: ninja.id.toString() }
+//         }
+//     })
+
+//     return {
+//         paths,
+//         fallback: false,
+//     }
+// }
+
+    try {
+       const {data} = await axios.get("https://jsonplaceholder.typicode.com/users")
+       //const data = res.data;
+        
+       const paths = data.map((ninja: any) => {
         return {
-            params: { id: ninja.id.toString() }
+            params: {id: ninja.id.toString()}
         }
-    })
+       })
 
-    return {
+       return {
         paths,
-        fallback: false,
+        fallback: false
+       }
+
+       } catch (error) {
+        console.log(error)
+        return {
+            paths: [],
+            fallback: false,
+           }
+
     }
+
 }
+    
+
+
 
 export const getStaticProps = async(context: any) => {
-    const id = context.params.id;
-    const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
-    const data = await res.json();
+    try {
+        const id = context.params.id;
+        const {data} = await axios.get("https://jsonplaceholder.typicode.com/users/" + id);
+        //const data = await res.data;
+        
 
     return {
         props: {ninjas: data}
     }
+
+   
+}  catch (error) {
+    console.log(error)
+    return {
+        props: []
+    }
+}
+
 }
 
 const Details = ({ninjas}: any) => {
+    console.log(ninjas);
     return (
         <div>
             <h1>{ ninjas.name }</h1>
