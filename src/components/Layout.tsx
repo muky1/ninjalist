17ca4a51bb654966ba53b0103eafa78c93cc912a
muky1 <muhamed.begic01@gmail.com>
2023-04-04 15:30:12 +0200

    rendering the login page conditionally

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,21 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { isLoggedInState } from "../pages/atoms/isLoggedInState";
+import { useRecoilValue } from "recoil";
+import LoginPage from "@/pages/auth";
 
 const Layout = ({ children }: any) => {
-    return ( 
-        <div className="content">
+    const isLoggedIn = useRecoilValue(isLoggedInState);
+
+    return (
+        <div>{isLoggedIn ? (
+            <div className="content">
             <Navbar/>
             { children }
             <Footer/>
         </div>
+        ) : <LoginPage/> }</div> 
+        
      );
 }
  
